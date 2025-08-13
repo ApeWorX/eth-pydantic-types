b0102bc35ab1e95eaa7f1239bfac4db3694a2d5e
Ilias Tsatiris <eliastsatiris@gmail.com>
2025-08-13 12:34:25 +0300

    test: extra assert on instance type

--- tests/hex/test_typehints.py
+++ tests/hex/test_typehints.py
@@ -6,3 +6,4 @@ from eth_pydantic_types.hex.bytes import HexBytes32
 def test_hexbytes_fromhex():
     x = HexBytes32.fromhex("00" * 32)
     assert_type(x, HexBytes32)
+    assert isinstance(x, HexBytes32)
